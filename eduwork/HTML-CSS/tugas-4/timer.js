05b4d379311b12783af684626f9f46cee9b16143
bagussona <bagussona87@gmail.com>
2021-10-29 18:24:53 +0700

    nitip eduwork v5

---
+++ eduwork/HTML-CSS/tugas-4/timer.js
@@ -0,0 +1,13 @@
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+    setInterval(displayNextImage, 3000);
+}
+
+var images = [], x = -1;
+images[0] = "/eduwork/HTML-CSS/tugas-4/img/eduwork/image1.jpg";
+images[1] = "/eduwork/HTML-CSS/tugas-4/img/eduwork/image2.jpg";
+images[2] = "/eduwork/HTML-CSS/tugas-4/img/eduwork/image3.jpg";
